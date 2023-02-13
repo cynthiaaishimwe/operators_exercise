cce62d72bd1980774b621a281c7cd9b79101dec5
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-02-13 11:38:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project
+# operators_exercise
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,62 @@
+fun main() {
+    var num1 = 12
+    var num2 = 2
+    var sum = num1 + num2
+    println(sum)
+
+    var num3 = 4
+    var num4 = 2
+    var difference = num3 - num4
+    println(difference)
+
+    var num5 = 18
+    var num6 = 2
+    var division = num5 / num6
+    println(division)
+
+    var num7 = 2
+    var num8 = 2
+    var product = num7 * num8
+    println(product)
+
+    var num9 = 19
+    var num10 = 2
+    var remainder = num9 % num10
+    println(remainder)
+
+    var phone = "iphone "
+    var price = 100000
+    var phoneprice = "iphone ";100000
+    println(phone + price)
+
+    var greetings = "hello "
+    var name = "ada"
+    var greetingsname = "helloada"
+    println(greetings+name)
+
+    addition(12,3)
+    difference(20,12)
+    product(2,2)
+    remainder(21,5)
+
+
+}
+
+     fun addition(num1:Int,numb2:Int){
+         var addition =(num1+numb2)
+         println(addition)
+
+     }
+     fun difference(number3:Int,number4:Int){
+         var difference = (number3-number4)
+         println(difference)
+
+     }
+fun product(numer3:Int,number4:Int){
+    var product = (numer3 * number4)
+    println(product)}
+
+    fun remainder(numb4:Int,numb5:Int){
+        var remainder = (numb4 % numb5)
+        println(remainder)
+}
